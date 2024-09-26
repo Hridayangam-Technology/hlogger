88b33975f835e09a4a7e2570366563aae2dac9e1
Aayush Dip Giri <aayushdip@Aayushs-MacBook-Pro.local>
2024-09-26 16:14:13 +0545

    feat: added logger class in hlogger.py

---
+++ src/hlogger/constants.py
@@ -0,0 +1,8 @@
+LOGGER_COLORS: dict[str, str] = {
+    "DEBUG": "\033[92m",  # Green
+    "INFO": "\033[90m",  # Grey
+    "WARNING": "\033[93m",  # Yellow
+    "ERROR": "\033[91m",  # Red
+    "CRITICAL": "\033[91m",  # Red
+    "RESET": "\033[0m",  # Reset color
+}
